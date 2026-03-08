96459e6bb1768959f860ab431efe64106ae0ed4c
thangbodoiqua <dluy2406@gmail.com>
2026-03-08 09:29:46 +0700

    initial config

---
+++ src/main/java/com/se2/htmlcsslearning/service/GradingService.java
@@ -0,0 +1,4 @@
+package com.se2.htmlcsslearning.service;
+
+public interface GradingService {
+}
---
+++ src/main/java/com/se2/htmlcsslearning/service/impl/GradingServiceImpl.java
@@ -0,0 +1,6 @@
+package com.se2.htmlcsslearning.service.impl;
+
+import main.java.com.se2.htmlcsslearning.service.GradingService;
+
+public class GradingServiceImpl implements GradingService {
+}
