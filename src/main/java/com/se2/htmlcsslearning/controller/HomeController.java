23c946154fdd4002ecee1176769340816a061752
Manhlee24 <lehongmanhub123@gmail.com>
2026-03-26 21:43:57 +0700

    add html and css lessons onto feature-view-lesson

--- src/main/java/com/se2/htmlcsslearning/controller/HomeController.java
+++ src/main/java/com/se2/htmlcsslearning/controller/HomeController.java
@@ -1,4 +1,12 @@
 package com.se2.htmlcsslearning.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
 }
